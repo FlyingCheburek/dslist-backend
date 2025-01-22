884db7264d4b6377ca0313ffb0fedb7b106a1438
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2025-01-22 20:29:47 -0300

    feat: GameService.java declaration
    Criação do serviço para obter todos os registros do banco de dados.

--- dslist/src/main/java/com/intensivojavaspring/dslist/repositories/GameRepository.java
+++ dslist/src/main/java/com/intensivojavaspring/dslist/repositories/GameRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.intensivojavaspring.dslist.entities.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-
+    
 }
