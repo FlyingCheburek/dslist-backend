103a1c138c3f233f9e135697160565d63aa34d50
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2025-01-22 19:46:28 -0300

    refactor: GameMinDTO

--- dslist/src/main/java/com/intensivojavaspring/dslist/dtos/GameMinDTO.java
+++ dslist/src/main/java/com/intensivojavaspring/dslist/dtos/GameMinDTO.java
@@ -3,5 +3,7 @@ package com.intensivojavaspring.dslist.dtos;
 import com.intensivojavaspring.dslist.entities.Game;
 
 public record GameMinDTO(Long id, String title, Integer year, String imgUrl, String shortDescription) {
-    
+    public GameMinDTO(final Game game) {
+        this(game.getId(), game.getTitle(), game.getYear(), game.getImgUrl(), game.getShortDescription());
+    }
 }
