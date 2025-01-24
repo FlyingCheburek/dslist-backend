5022b5874235b20e23cd841b5d1c31c165861e56
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2025-01-23 21:29:24 -0300

    refactor: GameController.java
    Importação não utilizada removida.

--- dslist/src/main/java/com/intensivojavaspring/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/intensivojavaspring/dslist/controllers/GameController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.intensivojavaspring.dslist.dtos.GameDTO;
 import com.intensivojavaspring.dslist.dtos.GameMinDTO;
 import com.intensivojavaspring.dslist.services.GameService;
 
@@ -18,5 +21,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll() {
         return gameService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO findById(@PathVariable final Long id) {
+        return gameService.findById(id);
     }
 }
