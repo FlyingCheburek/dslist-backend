fbeabcb1f1d288155dbd8cf6c5f7bc8737d4bde4
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2025-01-23 21:21:32 -0300

    feat: GameService findById
    Serviço para obter o GameDTO a partir de um Id específico.

--- dslist/src/main/java/com/intensivojavaspring/dslist/services/GameService.java
+++ dslist/src/main/java/com/intensivojavaspring/dslist/services/GameService.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.intensivojavaspring.dslist.dtos.GameDTO;
 import com.intensivojavaspring.dslist.dtos.GameMinDTO;
 import com.intensivojavaspring.dslist.repositories.GameRepository;
+
 import com.intensivojavaspring.dslist.entities.Game;
 
 @Service
@@ -13,8 +17,15 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(final Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
