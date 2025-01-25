55722405f5bf0c44583f2086ceedfd872272f1df
Thomas Pinheiro <thomashenriquesantospinheiro@gmail.com>
2025-01-25 18:40:08 -0300

    feat: move game inside list (change position)
    Criação do serviço para reposicionamento de um determinado jogo dentro de uma lista, com ID da fonte e ID de destino.

--- dslist/src/main/java/com/intensivojavaspring/dslist/services/GameListService.java
+++ dslist/src/main/java/com/intensivojavaspring/dslist/services/GameListService.java
@@ -9,16 +9,28 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.intensivojavaspring.dslist.dtos.GameListDTO;
 import com.intensivojavaspring.dslist.entities.GameList;
+import com.intensivojavaspring.dslist.projections.GameMinProjection;
 import com.intensivojavaspring.dslist.repositories.GameListRepository;
+import com.intensivojavaspring.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void moveGame(final Long listId, final Long srcIndex, final Long destIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection game = list.remove(srcIndex.intValue());
+        list.add(destIndex.intValue(), game);
     }
 }
